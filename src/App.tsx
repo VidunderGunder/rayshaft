81ec874a3d5e96efcc2100fbe29a6dd168ce5e42
Kristian Gunder Kramås <kristiankramas@outlook.com>
2025-02-02 20:16:17 +0100

    Refactor layout structure for input component

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,18 @@ export function App() {
 	useEscape();
 
 	return (
-		<div className="relative flex size-full items-center justify-center">
-			<input
-				type="text"
-				name="text"
-				placeholder="Gotta go fast..."
-				className="w-full rounded-2xl bg-gray-950/50 px-3.5 py-3 text-white"
-			/>
-			<span role="img" className="absolute right-4">
-				🦔💨
-			</span>
+		<div className="flex size-full items-start justify-center">
+			<div className="relative flex flex-1 items-center">
+				<input
+					type="text"
+					name="text"
+					placeholder="Gotta go fast..."
+					className="w-full rounded-2xl bg-gray-950/50 px-3.5 py-3 text-white"
+				/>
+				<span role="img" className="absolute right-4">
+					🦔💨
+				</span>
+			</div>
 		</div>
 	);
 }
