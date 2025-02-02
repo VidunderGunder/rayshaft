cb33b6093386cf6ead3a2e406ececd5601643b12
Kristian Gunder Kramås <kristiankramas@outlook.com>
2025-02-02 20:06:21 +0100

    Hacky, basic styling

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,16 @@ export function App() {
 	useEscape();
 
 	return (
-		<div className="size-full">
-			<form>
-				<input type="text" name="text" placeholder="Search..." />
-			</form>
+		<div className="relative flex size-full items-center justify-center">
+			<input
+				type="text"
+				name="text"
+				placeholder="Gotta go fast..."
+				className="w-full rounded-2xl bg-gray-950/50 px-3.5 py-3 text-white"
+			/>
+			<span role="img" className="absolute right-4">
+				🦔💨
+			</span>
 		</div>
 	);
 }
